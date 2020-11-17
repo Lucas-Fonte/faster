4d0854cfc78303e84d065b68d5dac2fca710984d
Lucas Souza da Fonte <fontelucas@yahoo.com.br>
2020-11-17 15:44:16 -0300

    [feature] added last video player validation

--- src/core/playback/PlaybackRateManager.ts
+++ src/core/playback/PlaybackRateManager.ts
@@ -65,58 +65,19 @@ class PlaybackRateManager {
     const code = `
         function setPlaybackSpeedFaster() {
           let playbackRate = ${playbackRate};
-          const [videoPlayer] = document.getElementsByTagName('video');
-  
-          if (!videoPlayer) {
+          const videoPlayerList = document.getElementsByTagName('video');
+
+          if (!videoPlayerList) {
             alert('Video not found');
             return;
           }
-          videoPlayer.playbackRate = playbackRate;
+          videoPlayerList[videoPlayerList.length - 1].playbackRate = playbackRate;
         }
   
         setPlaybackSpeedFaster();
         `;
     this.extensionOperator.executeScript(code);
   }
-
-  // private checkForPreviousValue(): void {
-  //   chrome.storage.local.get(['previousRate'], (result) => {
-  //     const currentRate =
-  //       result.previousRate ||
-  //       Number((<HTMLInputElement>this.playbackRateSlider).value).toFixed(1);
-  //     this.playbackRateOutput.innerHTML = `${currentRate}x`;
-  //     (<HTMLInputElement>this.playbackRateSlider).value = currentRate;
-  //   });
-  // }
-
-  // private changeRate(value: string | number): void {
-  //   chrome.storage.local.set({ previousRate: value }, () => {
-  //     this.playbackRateOutput.innerHTML = `${Number(value).toFixed(1)}x`;
-  //     this.executePlaybackRateChange(value);
-  //   });
-  // }
-
-  // private executePlaybackRateChange(playbackRate: string | number) {
-  //   chrome.tabs.executeScript(
-  //     {
-  //       code: `
-  //     function setPlaybackSpeedFaster() {
-  //       let playbackRate = ${playbackRate};
-  //       const [videoPlayer] = document.getElementsByTagName('video');
-
-  //       if (!videoPlayer) {
-  //         alert('Video not found');
-  //         return;
-  //       }
-  //       videoPlayer.playbackRate = playbackRate;
-  //     }
-
-  //     setPlaybackSpeedFaster();
-  //     `,
-  //     },
-  //     () => chrome.runtime.lastError
-  //   );
-  // }
 }
 
 export { PlaybackRateManager };
